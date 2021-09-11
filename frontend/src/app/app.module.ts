16a11353c4bd2049e1339be09a301d5589460144
Tiago Silva de Oliveira <tiagobsb84@gmail.com>
2021-09-11 00:26:27 -0300

    Criando projeto Games

---
+++ frontend/src/app/app.module.ts
@@ -0,0 +1,23 @@
+import { NgModule } from '@angular/core';
+import { BrowserModule } from '@angular/platform-browser';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavComponent } from './components/nav/nav.component';
+import { ContentComponent } from './components/content/content.component';
+
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavComponent,
+    ContentComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
